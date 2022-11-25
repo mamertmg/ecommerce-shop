3eea63fe12f47b8f996ca5684c58a33406378267
mamertmg <marquezgarcia.a@gmail.com>
2022-11-25 11:05:37 +0100

    added Product Context and Sidebar Context

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// product provider
+import ProductProvider from './context/ProductContext.js';
+// sidebar provider
+import SidebarProvider from './context/SidebarContext.js';
+// cart provider
+import CartProvider from './context/CartContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SidebarProvider>
+      <CartProvider>
+        <ProductProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ProductProvider>
+      </CartProvider>
+  </SidebarProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
